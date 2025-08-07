a44b424e816606f8f53f64931d625d7d5838519b
Anastasiia Medved <nastya0662793393@gmail.com>
2025-08-07 13:56:25 +0200

    Fix: updated contactType schema in model and Joi validation

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,32 +1,38 @@
 import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 20,
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['personal', 'business'],
+      default: 'personal',
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  phoneNumber: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 20,
-  },
-  contactType: {
-    type: String,
-    enum: ['personal', 'business'],
-    default: 'personal',
-  },
-  isFavourite: {
-    type: Boolean,
-    default: false,
-  },
-});
+);
 
 const Contact = model('contact', contactSchema);
 
--- src/schemas/contactsSchemas.js
+++ src/schemas/contactsSchemas.js
@@ -2,16 +2,16 @@ import Joi from 'joi';
 
 export const addContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
-  contactType: Joi.string().valid('personal', 'business').optional(),
+  email: Joi.string().email().required(),
   isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('personal', 'business').optional(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).optional(),
-  email: Joi.string().email().optional(),
   phoneNumber: Joi.string().min(3).max(20).optional(),
-  contactType: Joi.string().valid('personal', 'business').optional(),
+  email: Joi.string().email().optional(),
   isFavourite: Joi.boolean().optional(),
+  contactType: Joi.string().valid('personal', 'business').optional(),
 }).min(1);
